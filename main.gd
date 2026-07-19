e9c75ac3100b5a492ba8ee4caf843a7b8ad38f28
onewritescode <lalmohansingh83@gmail.com>
2026-07-19 12:16:15 +0530

    new man scene

--- main.gd
+++ main.gd
@@ -1,21 +1,64 @@
 extends TextureButton
+
 @onready var btnClickSound = $"../../buttonClick"
+
 var is_busy = false
 
-func _on_pressed() -> void:
+@onready var level_1_button = $"../TextureRect/level 1 btn"
+@onready var level_2_button = $"../TextureRect/level 2 btn"
+@onready var level_overlay = $"../TextureRect"
+@onready var play_btn = $"../play"
+
+func _ready():
+	level_overlay.visible = false
+
+	level_1_button.pressed.connect(_on_level1_button_pressed)
+	level_2_button.pressed.connect(_on_level2_button_pressed)
+	play_btn.pressed.connect(_on_play_button_pressed)
+
+func _on_play_button_pressed():
 	if is_busy:
 		return
+
 	is_busy = true
 	disabled = true
+
 	btnClickSound.play()
 	await btnClickSound.finished
-	get_tree().quit()
+	level_overlay.visible = true
+	is_busy = false
+	disabled = false
 
-func _on_play_pressed() -> void:
+func _on_pressed():
 	if is_busy:
 		return
+
 	is_busy = true
 	disabled = true
+
+	btnClickSound.play()
+	await btnClickSound.finished
+
+	get_tree().quit()
+
+func _on_level1_button_pressed():
+	if is_busy:
+		return
+
+	is_busy = true
+	disabled = true
+
 	btnClickSound.play()
 	await btnClickSound.finished
 	get_tree().change_scene_to_file("res://world.scn")
+
+func _on_level2_button_pressed():
+	if is_busy:
+		return
+
+	is_busy = true
+	disabled = true
+
+	btnClickSound.play()
+	await btnClickSound.finished
+	get_tree().change_scene_to_file("res://world2.tscn")
